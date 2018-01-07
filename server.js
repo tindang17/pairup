f3883bb88513aea309202d2a630527d8c34b273d
Tin Dang <tindang1710@gmail.com>
2018-01-06 16:01:58 -0800

    randomly choose a participant

--- server.js
+++ server.js
@@ -8,13 +8,52 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // set view engine to ejs
 app.set('view engine', 'ejs');
 
+const participants = [];
+const sortedParticipants = [];
 app.get('/', (req, res) => {
-  res.render('index');
+  const templateVars = {
+    participants,
+    sortedParticipants
+  };
+  res.render('index', templateVars);
 });
 
+function addParticipant (participant, res) {
+  participants.push(participant);
+  res.status(302).redirect("/");
+}
+// app.get('/sort', (req, res) => {
+//   const templateVars = {
+//     sortedParticipants
+//   }
+//   res.render('pairs', templateVars);
+// })
+app.post('/sort', (req, res) => {
+  randomlySort(participants, res);
+})
+function randomlySort (participants, res) {
+  const index = generateRandomNumber(participants.length);
+  if (participants.length === 0) {
+    res.send('no more participants');
+  } else {
+    sortedParticipants.push(participants.splice(index, 1));
+    res.redirect('/');
+  }
+}
+
+function generateRandomNumber (arrayLength) {
+  let number = Math.floor(Math.random() * arrayLength);
+  return number;
+}
 app.post('/name', (req, res) => {
-  console.log(req.body);
-  res.send('works');
+  const { participant } = req.body;
+  if (participant === '') {
+    res.send("field cannot be blanked");
+  } else if (participants.includes(participant)) {
+    res.send("participant has been added");
+  } else {
+    addParticipant(participant, res); 
+  }
 })
 http.createServer(app).listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
